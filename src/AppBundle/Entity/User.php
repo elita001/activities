62f29bb5358f42d5919b0f0c827e641777132355
Alexander Shutov <san4o528@gmail.com>
2018-04-04 14:06:16 +0400

    [FIX] User table doesn't work (it is a keyword)

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -8,7 +8,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 /**
  * User
  *
- * @ORM\Table(name="user")
+ * @ORM\Table(name="`user`")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\Entity\UserRepository")
  * @UniqueEntity(fields="email", message="Email already taken")
  */
